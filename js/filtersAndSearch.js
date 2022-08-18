f83ded999c2a6bfa6b1083bada43399166d852f1
azalex020204 <azalexxx02@gmail.com>
2022-08-17 19:02:54 -0700

    fix ++

--- js/filtersAndSearch.js
+++ js/filtersAndSearch.js
@@ -1,4 +1,4 @@
-// поліфіл для роботи випадаючого списку в браузерах що не підтримують forEach
+// polyfill for the operation of the drop-down list in browsers that do not support forEach
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = function (callback, thisArg) {
       thisArg = thisArg || window;
@@ -7,43 +7,7 @@ if (window.NodeList && !NodeList.prototype.forEach) {
       }
   };
 }
-
-const buttonMoreFoкOnePageInFooter = document.querySelector('#button');
 const moreInfoPage1 = document.querySelector('#content');
-
-
-// Кнопка показати більше у футері
-buttonMoreFoкOnePageInFooter.addEventListener('click', function () {
-  moreInfoPage1.classList.toggle('content__hidden');
-  if (moreInfoPage1.classList.contains('content__hidden')) {
-    buttonMoreFoкOnePageInFooter.textContent = 'More'
-  } else {
-    buttonMoreFoкOnePageInFooter.textContent = 'Less';
-  }
-})
-
-
-document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper){
-  const dropDownBtn = dropDownWrapper.querySelector('.textBox');
-  dropDownBtn.addEventListener('click', function() {
-  dropDownWrapper.classList.toggle('active');
-  
-  const dropDownList = dropDownWrapper.querySelector('.option__list');
-  const dropDownListItems = dropDownList.querySelectorAll('.option');
-
-  dropDownListItems.forEach(function (listItem){
-    listItem.addEventListener('click', function (e){
-      e.stopPropagation();
-      dropDownBtn.innerHTML = this.innerHTML;
-      dropDownBtn.focus();
-      dropDownWrapper.dropDownBtn.value = this.show.value;
-    })
-  })
-  })
-  function show(anything){
-      document.querySelector('.textBox').value = anything;
-    }
-});
 
 document.querySelectorAll('.card').forEach(function (dropDownInCards) {
   const dropDownBtnInCard = dropDownInCards.querySelector('#checkPriceID');
