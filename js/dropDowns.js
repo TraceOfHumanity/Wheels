4bf22b3c97c7bea88f871465683f0c3ab26198c2
azalex020204 <azalexxx02@gmail.com>
2022-08-22 15:47:10 -0700

    fix +

--- js/dropDowns.js
+++ js/dropDowns.js
@@ -1,10 +1,10 @@
 // polyfill for the operation of the drop-down list in browsers that do not support forEach=========================================
 if (window.NodeList && !NodeList.prototype.forEach) {
   NodeList.prototype.forEach = function (callback, thisArg) {
-      thisArg = thisArg || window;
-      for (let i = 0; i < this.length; i++) {
-          callback.call(thisArg, this[i], i, this);
-      }
+    thisArg = thisArg || window;
+    for (let i = 0; i < this.length; i++) {
+      callback.call(thisArg, this[i], i, this);
+    }
   };
 }
 
@@ -12,27 +12,30 @@ const buttonMoreFoкOnePageInFooter = document.querySelector('#button');
 const moreInfoPage1 = document.querySelector('#content');
 
 // dropDowns in tire2-1921.html===================================================================================================================
-document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper){
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
   const dropDownBtn = dropDownWrapper.querySelector('.textBox');
-  dropDownBtn.addEventListener('click', function() {
-  dropDownWrapper.classList.toggle('active');
-  
-  const dropDownList = dropDownWrapper.querySelector('.option__list');
-  const dropDownListItems = dropDownList.querySelectorAll('.option');
+  dropDownBtn.addEventListener('click', function () {
+    dropDownWrapper.classList.toggle('active');
 
-  dropDownListItems.forEach(function (listItem){
-    listItem.addEventListener('click', function (e){
-      e.stopPropagation();
-      listItem.innerHTML = this.innerHTML;
-      dropDownBtn.focus();
-      listItem.dropDownBtn.value = this.show.value;
+    const dropDownList = dropDownWrapper.querySelector('.option__list');
+    const dropDownListItems = dropDownList.querySelectorAll('.option');
+
+    dropDownListItems.forEach(function (listItem) {
+      listItem.addEventListener('click', function (e) {
+        e.stopPropagation();
+        listItem.innerHTML = this.innerHTML;
+        dropDownBtn.focus();
+        listItem.dropDownBtn.value = this.show.value;
+      })
     })
   })
-  })
-  function show(anything){
-      document.querySelector('.textBox').value = anything;
-    }
+  function show(anything) {
+    document.querySelector('.textBox').value = anything;
+  }
 });
+function show(anything) {
+  document.querySelector('.textBox').value = anything
+}
 
 
 
